f3ea068d42daf80ecab32593012d3897d73e1916
Akhilesh Gupta <imakhilesh0205@gmail.com>
2026-07-15 12:08:16 +0530

    One more floyds algo

---
+++ Linked-List/LoopLength_LL.py
@@ -0,0 +1,36 @@
+from LL_definition import LinkedList, printLL
+#it also implements floys fast and slow pointer and on that counts the length of Loop.
+def findLengthOfLoop(head):
+    #optimize floyds algo
+    slow = head
+    fast = head
+    while fast != None and fast.next!= None:
+        slow = slow.next 
+        fast = fast.next.next
+        if slow == fast:
+            #they have collided
+            #now counting length.
+            slow = slow.next
+            length = 1
+            while slow != fast:
+                slow = slow.next
+                length += 1
+            return length
+    return 0
+
+if __name__ == "__main__":
+    node1 = LinkedList(1)
+    node2 = LinkedList(2)
+    node3 = LinkedList(3)
+    node4 = LinkedList(4)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2 #loop
+    
+    head = node1
+
+    print("Length of Loop in LL:")
+    length = findLengthOfLoop(head)
+    print(length)
